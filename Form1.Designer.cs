4f3c9700981d994c863997cddf48b663266e1390
Emmanuel Aghan <aghan.odhiambo@strathmore.edu>
2020-05-05 05:53:21 +0300

    Login form update Username and password Login button timer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,25 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.txtpass);
+            this.panel1.Controls.Add(this.txtuser);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
@@ -54,16 +58,57 @@
             this.panel1.Size = new System.Drawing.Size(1342, 744);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // label3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(514, 289);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(378, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Username or email address";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(549, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(307, 111);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "&Login";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(731, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(556, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtpass
+            // 
+            this.txtpass.BackColor = System.Drawing.Color.DarkKhaki;
+            this.txtpass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txtpass.Location = new System.Drawing.Point(520, 425);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(360, 24);
+            this.txtpass.TabIndex = 3;
+            // 
+            // txtuser
+            // 
+            this.txtuser.BackColor = System.Drawing.Color.DarkKhaki;
+            this.txtuser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuser.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txtuser.Location = new System.Drawing.Point(520, 324);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(360, 24);
+            this.txtuser.TabIndex = 2;
             // 
             // label2
             // 
@@ -76,55 +121,32 @@
             this.label2.Text = "Password";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.DarkKhaki;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.textBox1.Location = new System.Drawing.Point(520, 324);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(360, 24);
-            this.textBox1.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(514, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username or email address";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.BackColor = System.Drawing.Color.DarkKhaki;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.textBox2.Location = new System.Drawing.Point(520, 425);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(360, 24);
-            this.textBox2.TabIndex = 3;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Unicode MS", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(913, 431);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
             // 
-            // button1
+            // timer1
             // 
-            this.button1.Location = new System.Drawing.Point(556, 501);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 44);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(731, 501);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 44);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(549, 114);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(307, 111);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "&Login";
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 4000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -146,12 +168,14 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtuser;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,7 @@ namespace BankSys
             InitializeComponent();
         }
 
+        int count;
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -25,6 +26,39 @@ namespace BankSys
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string username, password;
+
+            username = txtuser.Text;
+            password = txtpass.Text;
+            count = count + 1;
+
+            if (count > 3)
+            {
+                MessageBox.Show("You have MORE THAN 3 attempted Logins. Please contact your SYSTEM ADMINISTRATOR.");
+                Application.Exit();
+            }
+
+            if (username == "username" && password == "password") 
+            {
+                label4.Text = "Log in successful";
+
+            }
+            else
+            {
+                label4.Text = "Invalid Username and password";
+
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label4.Text = "";
         }
     }
 }
